da8379c6d5fd1d7ab9474df7c72c15433211db18
Jhampier Tucno <yantucnoperez@gmail.com>
2025-01-30 12:47:49 -0500

    Sección 19: Comunicación entre Frontend React y Backend Spring

--- 16-backend-products/src/main/java/com/tucno/springboot/repositories/ProductRepository.java
+++ 16-backend-products/src/main/java/com/tucno/springboot/repositories/ProductRepository.java
@@ -3,7 +3,9 @@ package com.tucno.springboot.repositories;
 import com.tucno.springboot.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:5173")
 // La anotation @RepositoryRestResource nos permite exponer los metodos de este repositorio como un servicio REST de Spring Data Rest
 // El path nos permite definir la URL de acceso a este servicio REST
 // En este caso, la URL de acceso a este servicio REST será http://localhost:8080/api/products
